c327b04f38e8a178dd973b4a94edf838ae490e54
achuinard <tony@twansoftware.com>
2024-05-05 19:51:12 -0400

    Update

---
+++ src/pages/posts/[slug]/index.png.ts
@@ -0,0 +1,20 @@
+import type { APIRoute } from "astro";
+import { getCollection, type CollectionEntry } from "astro:content";
+import { generateOgImageForPost } from "@utils/generateOgImages";
+import { slug as slugger } from "github-slugger";
+
+export async function getStaticPaths() {
+  const posts = await getCollection("blog").then(p =>
+    p.filter(({ data }) => !data.draft && !data.ogImage)
+  );
+
+  return posts.map(post => ({
+    params: { slug: slugger(post.data.title) },
+    props: post,
+  }));
+}
+
+export const GET: APIRoute = async ({ props }) =>
+  new Response(await generateOgImageForPost(props as CollectionEntry<"blog">), {
+    headers: { "Content-Type": "image/png" },
+  });
