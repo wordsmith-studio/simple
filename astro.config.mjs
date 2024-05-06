55924ad21d629ef7461a872ca0e0e89619757213
achuinard <tony@twansoftware.com>
2024-05-05 20:07:13 -0400

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
+    site: SITE.website,
     integrations: [
         react(),
         sitemap(),
